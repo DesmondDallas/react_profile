9451f3b61d89ccdbd30d68700a6741faac4f2ec3
Desmond Dallas <35736130+DesmondDallas@users.noreply.github.com>
2023-02-13 05:34:41 +0000

    test - automatic deploy fixed

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -14,7 +14,7 @@ function Home() {
             </div>
             <div>
               <span>
-                <h2>I'm Desmond Test 2</h2>
+                <h2>I'm Desmond</h2>
               </span>
             </div>
           </div>
